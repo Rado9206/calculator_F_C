be690e977dc31cfa4a6e343fdb6c341849be2f91
Radek <rj.airoo@gmail.com>
2023-04-27 18:18:48 +0200

    modified script.js

--- js/script.js
+++ js/script.js
@@ -7,13 +7,45 @@ const firstValue = document.querySelector('.one')
 const secondValue = document.querySelector('.two')
 
 const swap = () => {
-	if(firstValue.textContent === '°C') {
+	if (firstValue.textContent === '°C') {
 		firstValue.textContent = '°F'
 		secondValue.textContent = '°C'
+		result.textContent = 'Podaj jakąś wartość.'
 	} else {
 		firstValue.textContent = '°C'
 		secondValue.textContent = '°F'
+		result.textContent = 'Podaj jakąś wartość.'
 	}
 }
 
+const countFtoC = () => {
+	farenheit = converter.value * 1.8 + 32
+	result.textContent = `${converter.value}°C to ${farenheit.toFixed(1)}°F `
+	converter.value = ''
+}
+const countCtoF = () => {
+	celtius = (converter.value - 32) / 1.8
+	result.textContent = `${converter.value}°F to ${celtius.toFixed(1)}°C `
+	converter.value = ''
+}
+
+const conversion = () => {
+	if (converter.value !== '') {
+		if (firstValue.textContent === '°C') {
+			countFtoC()
+		} else {
+			countCtoF()
+		}
+	} else {
+		result.textContent = 'Podaj jakąś wartość.'
+	}
+}
+
+const reset = () => {
+	result.textContent = 'Podaj jakąś wartość.'
+	converter.value = ''
+}
+
 changeBtn.addEventListener('click', swap)
+convBtn.addEventListener('click', conversion)
+resetBtn.addEventListener('click', reset)
